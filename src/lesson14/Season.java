d0081abbdcf251aa73e24f16070858de0d142598
zakhar <zakhar@mail.com>
2018-03-05 20:40:22 +0200

    change

---
+++ src/lesson14/Season.java
@@ -0,0 +1,8 @@
+package lesson14;
+
+/**
+ * Создать перечисление, содержащее названия времен года.
+ */
+public enum Season {
+    WINTER, SPRING, SUMMER, AUTUMN;
+}
---
+++ src/lesson14/SeasonDemo.java
@@ -0,0 +1,10 @@
+package lesson14;
+
+/**
+ * Создать  переменную содержащую ваше любимое время.
+ */
+public class SeasonDemo {
+    public static void main(String[] args) {
+        Season season = Season.SPRING;
+    }
+}
