560336b61b2923cf1521afa929d5f202062f7882
zakhar <zakhar@mail.com>
2018-03-05 20:51:47 +0200

    change

--- src/lesson14/SeasonDemo.java
+++ src/lesson14/SeasonDemo.java
@@ -2,9 +2,32 @@ package lesson14;
 
 /**
  * Создать  переменную содержащую ваше любимое время.
+ * Создать метод, который принимает на вход переменную созданного вами enum типа.
+ * Если значение равно Лето, выводим на консоль “Я люблю лето” и т.д.
+ * Используем оператор switch.
  */
 public class SeasonDemo {
     public static void main(String[] args) {
         Season season = Season.SPRING;
+        
+        printSeason(season);
+        printSeason(Season.SUMMER);
+    }
+
+    public static void printSeason(Season season) {
+        switch (season) {
+            case WINTER:
+                System.out.println("Люблю зиму");
+                break;
+            case SPRING:
+                System.out.println("Люблю весну");
+                break;
+            case SUMMER:
+                System.out.println("Люблю лето");
+                break;
+            case AUTUMN:
+                System.out.println("Люблю осень");
+                break;
+        }
     }
 }
