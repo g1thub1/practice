083a3926c3f616300cfa8cb55a0c631b1af61a90
zakhar <zakhar@mail.com>
2018-03-05 21:17:59 +0200

    change

--- src/lesson14/SeasonDemo.java
+++ src/lesson14/SeasonDemo.java
@@ -5,13 +5,28 @@ package lesson14;
  * Создать метод, который принимает на вход переменную созданного вами enum типа.
  * Если значение равно Лето, выводим на консоль “Я люблю лето” и т.д.
  * Используем оператор switch.
+ * В цикле распечатать  все времена года. Используем метод values().
+ * Создать  переменную содержащую ваше любимое время года с использованием метода valueOf().
  */
 public class SeasonDemo {
     public static void main(String[] args) {
         Season season = Season.SPRING;
-        
         printSeason(season);
         printSeason(Season.SUMMER);
+        values();
+        valueOf();
+    }
+
+    private static void valueOf() {
+        Season season1 = Season.valueOf("SPRING");
+        System.out.println(season1);
+    }
+
+    private static void values() {
+        Season[] seas = Season.values();
+        for (Season s : seas) {
+            System.out.println(s);
+        }
     }
 
     public static void printSeason(Season season) {
