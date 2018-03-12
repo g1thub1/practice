15eb5970bc1ae7f22104a71dc89015ff61520639
zakhar <zakhar@mail.com>
2018-03-12 21:23:59 +0200

    change

--- src/lesson15/StringDemo.java
+++ src/lesson15/StringDemo.java
@@ -5,12 +5,42 @@ package lesson15;
  * Проверить, заканчивается ли ваша строка подстрокой “!!!”. Используем метод String.endsWith().
  * Проверить, начинается ли ваша строка подстрокой “I like”. Используем метод String. startsWith().
  * Распечатать последний символ строки. Используем метод String.charAt().
+ * Найти позицию подстроки “Java” в строке “I like Java!!!”
+ * Заменить все символы “а” на “о”.
+ * Преобразуйте строку к верхнему регистру.
+ * Преобразуйте строку к нижнему регистру.
+ * Вырезать строку Java c помощью метода String.substring.
  */
 public class StringDemo {
     public static void main(String[] args) {
         varifyString("I like Java!!");
         strLength("I like abc");
         charAt();
+        find("I love Java!!");
+        registerr();
+        registerr1("I like Java!!");
+        subbString();
+    }
+
+    private static void subbString() {
+        System.out.println("I like Java!".substring(7));
+        System.out.println("I like Java!".substring(5, 11));
+    }
+
+    private static void registerr1(String str) {
+        System.out.println(str.replace('a', 'o'));
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+    }
+
+    private static void registerr() {
+        System.out.println("I like Avaj!!".replace('a', 'o'));
+        System.out.println("I like AVAJ!!".toLowerCase());
+        System.out.println("I like AVAJ!!".toUpperCase());
+    }
+
+    private static void find(String str) {
+        System.out.println(str.indexOf("love"));
     }
 
     private static void charAt() {
