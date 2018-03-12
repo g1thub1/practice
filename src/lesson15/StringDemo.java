fb3922e17ed1be3571e6a1f604960be4c8fc617c
zakhar <zakhar@mail.com>
2018-03-12 20:52:34 +0200

    change

--- src/lesson15/StringDemo.java
+++ src/lesson15/StringDemo.java
@@ -4,10 +4,17 @@ package lesson15;
  * Напишите метод, который принимает в качестве параметра любую строку, например “I like Java!!!”.
  * Проверить, заканчивается ли ваша строка подстрокой “!!!”. Используем метод String.endsWith().
  * Проверить, начинается ли ваша строка подстрокой “I like”. Используем метод String. startsWith().
+ * Распечатать последний символ строки. Используем метод String.charAt().
  */
 public class StringDemo {
     public static void main(String[] args) {
         varifyString("I like Java!!");
+        charAt();
+    }
+
+    private static void charAt() {
+        char ch = "I like Java!!".charAt(12);
+        System.out.println(ch);
     }
 
     public static void varifyString(String str) {
