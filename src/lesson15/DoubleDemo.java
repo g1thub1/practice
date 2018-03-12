30eb1b218b9e151aa6216a8bd3c1fff0338aaecf
zakhar <zakhar@mail.com>
2018-03-12 20:29:46 +0200

    change

--- src/lesson15/DoubleDemo.java
+++ src/lesson15/DoubleDemo.java
@@ -5,13 +5,17 @@ package lesson15;
  * Создайте объекты класса Double, используя методы valueOf().
  * Преобразовать значение типа String к типу double.
  * Используем метод Double.parseDouble().
+ * Заменить конструктор на упаковку.
  */
 public class DoubleDemo {
     public static void main(String[] args) {
         Double double1 = new Double((double) 2.5);
         Double double2 = new Double("2.55");
+        Double double6 = 3.35;
+        
         System.out.println(double1);
         System.out.println(double2);
+        System.out.println(double6);
 
         doubleValueOf();
         parseDouble();
@@ -26,7 +30,6 @@ public class DoubleDemo {
     private static void doubleValueOf() {
         Double double3 = Double.valueOf(4.5);
         Double double4 = Double.valueOf("4.55");
-
         System.out.println(double3);
         System.out.println(double4);
     }
