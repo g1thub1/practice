8bb828c6c797e777dcc8e6c2dda292203b51274f
zakhar <zakhar@mail.com>
2018-03-19 21:30:22 +0200

    change

---
+++ src/lesson17/ResourceBundleDemo.java
@@ -0,0 +1,30 @@
+package lesson17;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Создать properties файлы на русском и английским, содержащие названия изученных тем по Java.
+ * Создать метод, который получает в качестве параметра локаль и распечатывает всю информацию из properties файла.
+ * Используем метод ResourceBundle.keySet().
+ */
+public class ResourceBundleDemo {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        printInfo("", "");
+        printInfo("en", "US");
+        printInfo("uk", "UA");
+    }
+
+    private static void printInfo(String language, String country)
+            throws UnsupportedEncodingException {
+        Locale current = new Locale(language, country);
+        ResourceBundle rb = ResourceBundle.getBundle("topic", current);
+        for (String key : rb.keySet()) {
+            String value = rb.getString(key);
+            value = new String(value.getBytes("ISO-8859-1"), "UTF-8");
+            System.out.println(value);
+        }
+        System.out.println();
+    }
+}
